aa978edb9e1fec47433c496047ec2516b93f7853
sameeranihathe <sameeranihathe@gmail.com>
2018-04-16 17:43:08 +0530

    add-migration DefaultDatabaseColumnChanged2

---
+++ WebApi/Migrations/201804161206468_DefaultDatabaseColumnChanged2.cs
@@ -0,0 +1,16 @@
+namespace WebApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DefaultDatabaseColumnChanged2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
