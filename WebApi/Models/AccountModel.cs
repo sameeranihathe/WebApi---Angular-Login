abff38515245ad249235965fc817382cf3a59a76
sameeranihathe <sameeranihathe@gmail.com>
2018-04-16 20:12:07 +0530

    AccountsModel created

--- WebApi/Models/AccountModel.cs
+++ WebApi/Models/AccountModel.cs
@@ -7,5 +7,10 @@ namespace WebApi.Models
 {
     public class AccountModel
     {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastNmae { get; set; }
     }
 }
