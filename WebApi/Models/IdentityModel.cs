701123e07f4b9d22e6064c13652b219de52ef7ad
sameeranihathe <sameeranihathe@gmail.com>
2018-04-16 17:31:56 +0530

    add-migration DefaultDatabaseColumnChanged

--- WebApi/Models/IdentityModel.cs
+++ WebApi/Models/IdentityModel.cs
@@ -9,6 +9,8 @@ namespace WebApi.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
